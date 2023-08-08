e13eb2e41c486dcd0b7b2ab36c6842704cc4d80c
Kozoloskit <kozoloski@yahoo.com>
2023-08-08 21:01:30 +0200

    Homework1

---
+++ Homework1/Homework1/Homework1/StaticDb.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "Tomo",
+            "Ivana",
+            "Simon"
+        };
+    }
+}
