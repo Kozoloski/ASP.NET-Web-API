a1c532ff98b62d351e94d93260d4a210bf0550ca
Kozoloskit <kozoloski@yahoo.com>
2023-08-13 22:13:27 +0200

    Homework2

---
+++ Homework2/Homework2/Homework2/StaticDb.cs
@@ -0,0 +1,23 @@
+﻿using Homework2.Models;
+
+namespace Homework2
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books = new List<Book>
+        {
+            new Book()
+            {
+              Author = "Stephen King",
+              Title = "The Shining"
+            },
+            new Book()
+            {
+              Author = "Isaac Asimov",
+              Title = "Fondation"
+            },
+             
+        };
+
+    }
+}
